8b0cb56e04d9436d316a25c95237c7cc42d0ee66
bcroner <seekandwander@gmail.com>
2026-01-01 11:25:24 -0800

    Update 3SATBaseConsole.cpp

--- 3SATBaseConsole.cpp
+++ 3SATBaseConsole.cpp
@@ -641,6 +641,9 @@ bool SATSolver_isSat(SATSolver* s, __int64 chops, bool* sln) {
                 }
             }
         }
+
+        if (ix >= s->n - chops)
+            break;
     }
 
     delete[] is_t;
