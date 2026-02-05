60a2964ecc980fa24697fcc27bc3a477d33ceea8
bcroner <seekandwander@gmail.com>
2026-02-05 14:09:21 -0800

    Update 3SATBaseConsole.cpp

--- 3SATBaseConsole.cpp
+++ 3SATBaseConsole.cpp
@@ -251,15 +251,16 @@ bool bool_equals(bool* A, bool* B, __int64 n) {
 
 bool two_sat(__int64* lst_l_parm, __int64* lst_r_parm, __int64 k_parm, __int64 n_parm, bool * is_f, bool * is_t) {
 
+    for (__int64 i = 0; i < k_parm; i++)
+        printf_s("%lld: %lld %lld\n", i, lst_l_parm[i], lst_r_parm[i]);
+    printf_s("\n");
+
     __int64 counter = 2;
 
     __int64* encoding = new __int64[n_parm]; // from 2..n_parm to 2..n
-    __int64* decoding = new __int64[n_parm]; // from 2..n to 2..n_parm
 
-    for (__int64 i = 0; i < n_parm; i++) {
+    for (__int64 i = 0; i < n_parm; i++)
         encoding[i] = 0;
-        decoding[i] = 0;
-    }
 
     bool* used = new bool[n_parm];
 
@@ -275,7 +276,7 @@ bool two_sat(__int64* lst_l_parm, __int64* lst_r_parm, __int64 k_parm, __int64 n
         used[r_abs] = true;
     }
 
-    __int64 n = 1;
+    __int64 n = 0;
 
     for (__int64 i = 2; i < n_parm; i++)
         if (used[i])
@@ -284,7 +285,6 @@ bool two_sat(__int64* lst_l_parm, __int64* lst_r_parm, __int64 k_parm, __int64 n
     for (__int64 i = 2; i < n_parm; i++) {
         if (used[i]) {
             encoding[i] = counter;
-            decoding[counter] = i;
             counter++;
         }
     }
@@ -309,6 +309,10 @@ bool two_sat(__int64* lst_l_parm, __int64* lst_r_parm, __int64 k_parm, __int64 n
 
         counter_k++;
     }
+
+    for (__int64 i = 0; i < k_parm; i++)
+        printf_s("%lld: %lld %lld\n", i, lst_l[i], lst_r[i]);
+    printf_s("\n");
 
     __int64* false_implies_top = new __int64[n];
     __int64* false_implies_cap = new __int64[n];
@@ -335,14 +339,14 @@ bool two_sat(__int64* lst_l_parm, __int64* lst_r_parm, __int64 k_parm, __int64 n
         __int64 r_abs = lst_r[i] < 0 ? -lst_r[i] : lst_r[i];
 
         if (lst_l[i] < 0)
-            simp_vector_append(&(true_implies[i]), &(true_implies_top[i]), &(true_implies_cap[i]), lst_r[i]);
+            simp_vector_append(&(true_implies[l_abs]), &(true_implies_top[l_abs]), &(true_implies_cap[l_abs]), lst_r[i]);
         else
-            simp_vector_append(&(false_implies[i]), &(false_implies_top[i]), &(false_implies_cap[i]), lst_r[i]);
+            simp_vector_append(&(false_implies[l_abs]), &(false_implies_top[l_abs]), &(false_implies_cap[l_abs]), lst_r[i]);
 
         if (lst_r[i] < 0)
-            simp_vector_append(&(true_implies[i]), &(true_implies_top[i]), &(true_implies_cap[i]), lst_l[i]);
+            simp_vector_append(&(true_implies[r_abs]), &(true_implies_top[r_abs]), &(true_implies_cap[r_abs]), lst_l[i]);
         else
-            simp_vector_append(&(false_implies[i]), &(false_implies_top[i]), &(false_implies_cap[i]), lst_l[i]);
+            simp_vector_append(&(false_implies[r_abs]), &(false_implies_top[r_abs]), &(false_implies_cap[r_abs]), lst_l[i]);
             
     }
 
@@ -389,7 +393,7 @@ bool two_sat(__int64* lst_l_parm, __int64* lst_r_parm, __int64 k_parm, __int64 n
                 trues[val_abs] = true;
         }
 
-        for (__int64 i = n - 1; i >= ix; i--)
+        for (__int64 i = ix; i < n; i++)
             if (Z[i]) {
                 for (__int64 j = 0; j < true_implies_top[i] + 1; j++) {
 
@@ -472,7 +476,6 @@ bool two_sat(__int64* lst_l_parm, __int64* lst_r_parm, __int64 k_parm, __int64 n
     delete[] true_implies;
 
     delete[] encoding;
-    delete[] decoding;
 
     delete[] used;
 
@@ -530,6 +533,13 @@ bool SATSolver_isSat(SATSolver* s, bool* sln) {
             return false;
         }
 
+    for (__int64 i = 2; i < s->n; i++)
+        if (always_f[i] && always_t[i]) {
+            delete[] always_f;
+            delete[] always_t;
+            return false;
+        }
+
     __int64 count_always_t = 0;
     __int64 count_always_f = 0;
 
@@ -574,46 +584,63 @@ bool SATSolver_isSat(SATSolver* s, bool* sln) {
 
         for (__int64 i = s->n - 1; i >= ix; i--)
             if (s->Z[i])
-                size_2sat += s->cd_sizes_f[i];
-            else
                 size_2sat += s->cd_sizes_t[i];
+            else
+                size_2sat += s->cd_sizes_f[i];
 
         __int64* cd_2sat_l = new __int64[size_2sat];
         __int64* cd_2sat_r = new __int64[size_2sat];
         __int64 cd_2sat_cur_sz_f = 0;
         __int64 cd_2sat_cur_sz_t = 0;
 
-        for (__int64 i = 2; i < s->n; i++) {
-            if (always_f[i]) {
-                cd_2sat_l[cd_2sat_cur_sz_f] = i;
-                cd_2sat_r[cd_2sat_cur_sz_f] = FALSE_3SAT;
-                cd_2sat_cur_sz_f++;
-            }
-            if (always_t[i]) {
-                cd_2sat_l[cd_2sat_cur_sz_t] = i;
-                cd_2sat_r[cd_2sat_cur_sz_t] = FALSE_3SAT;
-                cd_2sat_cur_sz_t++;
-            }
-        }
-
-        for (__int64 i = s->n - 1; i >= ix; i--) {
+        for (__int64 i = ix; i < s->n; i++) {
             if (s->Z[i]) {
-                for (__int64 j = 0; j < s->cd_sizes_f[i]; j++) {
-                    cd_2sat_l[cd_2sat_cur_sz_f + cd_2sat_cur_sz_t + j] = s->cdopcelll_f[i][j];
-                    cd_2sat_r[cd_2sat_cur_sz_f + cd_2sat_cur_sz_t + j] = s->cdopcellr_f[i][j];
+                for (__int64 j = 0; j < s->cd_sizes_t[i]; j++) {
+
+                    if (s->cdopcelll_t[i][j] == FALSE_3SAT || s->cdopcellr_t[i][j] == FALSE_3SAT) {
+
+                        __int64 val = s->cdopcelll_t[i][j] == FALSE_3SAT ? s->cdopcellr_t[i][j] : s->cdopcelll_t[i][j];
+                        __int64 val_abs = val < 0 ? -val : val;
+
+                        if (val < 0)
+                            is_f[val_abs] = true;
+                        else
+                            is_t[val_abs] = true;
+                    }
+                    else {
+
+                        cd_2sat_l[cd_2sat_cur_sz_f + cd_2sat_cur_sz_t] = s->cdopcelll_t[i][j];
+                        cd_2sat_r[cd_2sat_cur_sz_f + cd_2sat_cur_sz_t] = s->cdopcellr_t[i][j];
+
+                        cd_2sat_cur_sz_t++;
+                    }
                 }
-                cd_2sat_cur_sz_f += s->cd_sizes_f[i];
             }
             else {
-                for (__int64 j = 0; j < s->cd_sizes_t[i]; j++) {
-                    cd_2sat_l[cd_2sat_cur_sz_f + cd_2sat_cur_sz_t + j] = s->cdopcelll_t[i][j];
-                    cd_2sat_r[cd_2sat_cur_sz_f + cd_2sat_cur_sz_t + j] = s->cdopcellr_t[i][j];
+                for (__int64 j = 0; j < s->cd_sizes_f[i]; j++) {
+
+                    if (s->cdopcelll_f[i][j] == FALSE_3SAT || s->cdopcellr_f[i][j] == FALSE_3SAT) {
+
+                        __int64 val = s->cdopcelll_f[i][j] == FALSE_3SAT ? s->cdopcellr_f[i][j] : s->cdopcelll_f[i][j];
+                        __int64 val_abs = val < 0 ? -val : val;
+
+                        if (val < 0)
+                            is_f[val_abs] = true;
+                        else
+                            is_t[val_abs] = true;
+                    }
+                    else {
+
+                        cd_2sat_l[cd_2sat_cur_sz_f + cd_2sat_cur_sz_t] = s->cdopcelll_f[i][j];
+                        cd_2sat_r[cd_2sat_cur_sz_f + cd_2sat_cur_sz_t] = s->cdopcellr_f[i][j];
+
+                        cd_2sat_cur_sz_f++;
+                    }
                 }
-                cd_2sat_cur_sz_t += s->cd_sizes_t[i];
             }
         }
 
-        bool is_2sat_sat = two_sat(cd_2sat_l, cd_2sat_r, cd_2sat_cur_sz_f + cd_2sat_cur_sz_t, s->n, is_f, is_t);
+        bool is_2sat_sat = (cd_2sat_cur_sz_f + cd_2sat_cur_sz_t) == 0 ? true : two_sat(cd_2sat_l, cd_2sat_r, cd_2sat_cur_sz_f + cd_2sat_cur_sz_t, s->n, is_f, is_t);
 
         delete[] cd_2sat_l;
         delete[] cd_2sat_r;
